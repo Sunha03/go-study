265f7c0bd2bd2c108846bcd4fd91372b4cd519c1
Sunha03 <ahnus123@gmail.com>
2021-12-11 22:24:50 +0900

    [grpcserver] grpc 서버 구현

---
+++ src/grpcserver/data/user.go
@@ -0,0 +1,39 @@
+package data
+
+import (
+	userpb "grpcserver/pb"
+)
+
+// UserData : user 데이터 셋팅
+var UserData = []*userpb.UserMessage {
+	{
+		UserId: "1",
+		Name: "Henry",
+		PhoneNumber: "01012341234",
+		Age: 20,
+	},
+	{
+		UserId: "2",
+		Name: "Micheal",
+		PhoneNumber: "01056785678",
+		Age: 21,
+	},
+	{
+		UserId: "3",
+		Name: "Jessie",
+		PhoneNumber: "01090129012",
+		Age: 22,
+	},
+	{
+		UserId: "4",
+		Name: "Max",
+		PhoneNumber: "01045129012",
+		Age: 23,
+	},
+	{
+		UserId: "5",
+		Name: "Tony",
+		PhoneNumber: "01012345678",
+		Age: 24,
+	},
+}
--- src/grpcserver/main.go
+++ src/grpcserver/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	"net"
+	"context"
 
+	"grpcserver/data"
+	userpb "grpcserver/pb"
 	"google.golang.org/grpc"
 )
 
 const portNumber = "8000" // port 번호
+
+type userServer struct {
+	userpb.UserServer
+}
 
 func main() {
 	listen, err := net.Listen("tcp", ":"+portNumber)
@@ -15,10 +22,42 @@ func main() {
 		log.Fatalf("Failed to listen : %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer()		// User service를 GRPC server에 등록
+	userpb.RegisterUserServer(grpcServer, &userServer{})
 
 	log.Printf("Start GRPC server on %s port", portNumber)
 	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("Failed to Serve : %s", err)
 	}
+}
+
+// GetUser : user 상세 조회
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userId := req.UserId
+
+	var userMessage *userpb.UserMessage
+	for _, u := range data.UserData {
+		if u.UserId != userId {
+			continue
+		} else {
+			userMessage = u
+			break
+		}
+	}
+
+	return &userpb.GetUserResponse {
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers : user 리스트 조회
+func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+	userMessages := make([]*userpb.UserMessage, len(data.UserData))
+	for i, user := range data.UserData {
+		userMessages[i] = user
+	}
+
+	return &userpb.ListUsersResponse {
+		UserMessages: userMessages,
+	}, nil
 }
